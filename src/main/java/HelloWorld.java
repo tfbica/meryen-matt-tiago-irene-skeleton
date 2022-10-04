eadda183a368121e7a2a8ab8dad0bf1efa0d0dfa
tiagobica <tiago.bica@codurance.com>
2022-10-04 13:15:31 +0100

    Change port

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        port(8080);
         get("/hello", (req, res) -> "Hello World");
     }
 }
