c1a1277e99c92d4615b126d5a028660f14189321
tiagobica <tiago.bica@codurance.com>
2022-10-04 13:28:16 +0100

    Get port from environment

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,14 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        port(8080);
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello World");
+    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
