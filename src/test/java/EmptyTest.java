d2b6f43880e0417c0a48c016d4713c20a4bfbb6e
MattW <mattnwarnock@yahoo.co.uk>
2022-10-04 10:24:14 +0100

    Hello Route with empty test

---
+++ src/test/java/EmptyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmptyTest {
+    @Test
+    void test() {
+        assertEquals(1,1);
+    }
+}
