4ad10c393105f9fd8e6dbcd8661547f102105b78
MattW <mattnwarnock@yahoo.co.uk>
2022-10-04 10:53:17 +0100

    Plugin for test run in package

--- src/test/java/EmptyTest.java
+++ src/test/java/EmptyTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class EmptyTest {
     @Test
     void test() {
